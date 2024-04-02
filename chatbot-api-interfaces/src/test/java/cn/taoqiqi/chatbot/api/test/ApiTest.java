862b63e35ac24829a7889821dc286c9740ce95fb
桃祺祺 <1804538916@qq.com>
2024-04-02 23:42:41 +0800

    接口测试

--- chatbot-api-interfaces/src/test/java/cn/taoqiqi/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/cn/taoqiqi/chatbot/api/test/ApiTest.java
@@ -1,7 +1,68 @@
 package cn.taoqiqi.chatbot.api.test;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+
 /**
  * @author : wyq
  */
 public class ApiTest {
+
+    @Test
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/15555515842142/topics?scope=unanswered_questions&count=20");
+
+        get.addHeader("cookie", "sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22189c3c938fc1111-06db32a35cc72e8-7c546c76-1638720-189c3c938fd14e1%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E5%BC%95%E8%8D%90%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fwww.yuque.com%2F%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTg5YzNjOTM4ZmMxMTExLTA2ZGIzMmEzNWNjNzJlOC03YzU0NmM3Ni0xNjM4NzIwLTE4OWMzYzkzOGZkMTRlMSJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%2C%22%24device_id%22%3A%22189c3c938fc1111-06db32a35cc72e8-7c546c76-1638720-189c3c938fd14e1%22%7D; zsxq_access_token=0F8A38B3-D9C3-68C0-51DC-A686A3ECC5B6_AEBD09C0396ECDA1; zsxqsessionid=6ce2128d37b4ec37caa35c657eaae63c; abtest_env=product");
+
+        get.addHeader("Content-Type", "application/json;charset=utf8");
+
+        CloseableHttpResponse response = httpClient.execute(get);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
+
+    @Test
+    public void answer() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost("https://api.zsxq.com/v2/topics/4844812588282848/answer");
+        post.addHeader("cookie", "sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22189c3c938fc1111-06db32a35cc72e8-7c546c76-1638720-189c3c938fd14e1%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E5%BC%95%E8%8D%90%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fwww.yuque.com%2F%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTg5YzNjOTM4ZmMxMTExLTA2ZGIzMmEzNWNjNzJlOC03YzU0NmM3Ni0xNjM4NzIwLTE4OWMzYzkzOGZkMTRlMSJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%2C%22%24device_id%22%3A%22189c3c938fc1111-06db32a35cc72e8-7c546c76-1638720-189c3c938fd14e1%22%7D; zsxq_access_token=0F8A38B3-D9C3-68C0-51DC-A686A3ECC5B6_AEBD09C0396ECDA1; zsxqsessionid=6ce2128d37b4ec37caa35c657eaae63c; abtest_env=product");
+
+        post.addHeader("Content-Type", "application/json;charset=utf8");
+
+        String paramJson = "{\n" +
+                "  \"req_data\": {\n" +
+                "    \"text\": \"自己去百度！\\n\",\n" +
+                "    \"image_ids\": [],\n" +
+                "    \"silenced\": false\n" +
+                "  }\n" +
+                "}";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
 }
